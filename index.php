3159f810a700adcad7eefdf02750a535caf6e214
Mastermori <mastermoriii@gmail.com>
2016-10-15 11:24:24 +0200

    added testmap

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Map</title>
+	<link rel="stylesheet" href="lib/leaflet.css" />
+	<script src="hlib/leaflet.js"></script>
+</head>
+<body>
+
+	<div id="mapid"></div>
+
+	<script type="text/javascript">
+		var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+	</script>
+</body>
+</html>

426ff7a2a56db9f9bce3a413fb3d3975931bbf85
Mastermori <mastermoriii@gmail.com>
2016-10-15 11:55:15 +0200

    added OpenStreetMap to index.php

--- index.php
+++ index.php
@@ -11,6 +11,8 @@
 
 	<script type="text/javascript">
 		var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+		var OpenStreetMap = L.tileLayer('http://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png', { maxZoom: 15, minZoom: 5, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>' }).addTo(mymap);
 	</script>
+	
 </body>
 </html>

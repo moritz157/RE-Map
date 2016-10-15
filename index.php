b4371e2e32f44eed639bdfd1ec2286becdbe3b69
Mastermori <mastermoriii@gmail.com>
2016-10-15 12:11:50 +0200

    moved libs

--- index.php
+++ index.php
@@ -2,17 +2,19 @@
 <html>
 <head>
 	<title>Map</title>
-	<link rel="stylesheet" href="lib/leaflet.css" />
-	<script src="hlib/leaflet.js"></script>
+	<link rel="stylesheet" href="leaflet/leaflet.css" />
+	<link rel="stylesheet" href="style.css" />
+	<script src="leaflet/leaflet.js"></script>
 </head>
 <body>
 
-	<div id="mapid"></div>
+	
+<div id="map"></div>
 
 	<script type="text/javascript">
-		var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-		var OpenStreetMap = L.tileLayer('http://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png', { maxZoom: 15, minZoom: 5, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>' }).addTo(mymap);
+		var map = L.map('map').setView([51.505, -0.09], 13);
+		var OpenStreetMap = L.tileLayer('http://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png', { maxZoom: 15, minZoom: 5, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>' }).addTo(map);
 	</script>
-	
+
 </body>
 </html>

d31ba82a3c407978096b9cc5d38e62f9220a392c
vlt. Mondanzo <sackboysteve1@gmail.com>
2016-10-15 20:04:08 +0200

    renamed FloPaser.php to FloParser.php and fixed wrong details

--- FloJson_Parser.php
+++ FloJson_Parser.php
@@ -22,14 +22,14 @@ function read_json($json, $final_json){
         $final_array['comment'] = "Keine Angabe";
         $final_array['website'] = "Keine Angabe";
         $final_array['email'] = "Keine Angabe";
-        $final_array['lat'] = $item['latitude'];
-        $final_array['lon'] = $item['longitude'];
+        $final_array['lat'] = str_replace(",", ".", $item['latitude']);
+        $final_array['lon'] = str_replace(",", ".", $item['longitude']);
 
         if(!empty($item['location'])) $final_array['name'] = $item['location'];
         if(!empty($item['tage'])) $final_array['days'] = $item['tage'];
         if(!empty($item['betreiber'])) $final_array['owner'] = $item['betreiber'];
         if(!empty($item['zeit'])) $final_array['time'] = $item['zeit'];
-        if(!empty($item['comment'])) $final_array['comment'] = $item['bemerkung'];
+        if(!empty($item['bemerkung'])) $final_array['comment'] = $item['bemerkung'];
         if(!empty($item['www'])) $final_array['website'] = $item['www'];
         if(!empty($item['email'])) $final_array['email'] = $item['email'];
         array_push($final_json, $final_array);
